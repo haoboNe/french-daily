c875b8a83f8f0e6d5a28e323795dc505f4749a07
haoboNe <55165512+haoboNe@users.noreply.github.com>
2023-07-13 22:16:14 +0200

    Create collect_french_phrase.py

---
+++ .github/workflows/collect_french_phrase.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 发起 HTTP 请求获取网页内容
+response = requests.get('https://www.frenchlearner.com/french-phrases/')
+html_content = response.text
+
+# 使用 BeautifulSoup 解析网页内容
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# 在网页中查找法语短语元素
+phrase_element = soup.select_one('.post-content blockquote')
+
+# 提取法语短语文本
+phrase = phrase_element.get_text(strip=True)
+
+# 将法语短语写入文件
+with open('phrase.txt', 'w', encoding='utf-8') as file:
+    file.write(phrase)
